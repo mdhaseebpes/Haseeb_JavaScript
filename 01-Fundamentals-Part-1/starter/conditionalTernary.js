d47731aeaf682627d65ffd750186aed2c389ed2a
Haseeb <mdhaseebpes@gmail.com>
2025-03-08 02:51:36 +0530

    switch, ternary code

---
+++ 01-Fundamentals-Part-1/starter/conditionalTernary.js
@@ -0,0 +1,13 @@
+//Expression == anything that produces a value
+//Statement  == having declaration and define the entire sentence 
+
+const age = 37;
+age >= 30 ? console.log("Eligible for leadership") : console.log("Eligible for employee");
+
+const age1 = 19;
+const drink = age1 >= 18 ? 'Biryani 🍗' : 'Ghee rice 🍚';
+console.log(drink);
+
+const age2 = 17;
+console.log(`I like to eat ${age2 >=18 ? 'Biryani' : 'Ghee rice'}`);
+
---
+++ 01-Fundamentals-Part-1/starter/switchExample.js
@@ -0,0 +1,53 @@
+const day = 'sunday';
+
+switch(day.toLowerCase()) {
+    case 'monday': //day === 'monday'
+        console.log("java script learning videos ");
+      break;
+      case 'tuesday':
+        console.log("WebDriver IO learning videos");
+        console.log("office work ");
+        break;
+        case 'wednesday':
+        case 'thursday':
+        console.log("Javascript and WebDriver IO learning videos");
+        console.log("office work ");
+        break;
+        case 'friday':
+        console.log("prayers");
+        console.log("office work ");
+        break;
+        case 'saturday' :
+        console.log("fitness and MMA");
+        console.log("Home visit");
+        break;
+        case 'sunday' :
+        console.log("Relax with family");
+            break;
+            default : console.log("continue journey");
+    break;
+       
+
+}
+
+// if(day ==='monday') {
+// console.log("java script learning videos ");
+// }else if (day ==='tuesday') {
+// console.log("WebDriver IO learning videos");
+// console.log("office work ");
+// } else if(day === 'wednesday' || day === 'thursday') {
+// console.log("Javascript and WebDriver IO learning videos");
+// console.log("office work ");
+// } else if (day === 'friday') {
+// console.log("prayers");
+// console.log("office work ");
+// } else if (day === 'saturday') {
+// console.log("fitness and MMA");
+// console.log("Home visit");
+// } else if (day === 'sunday') {
+// console.log("Relax with family");
+// } else {
+//     console.log("continue journey");
+// }
+
+
